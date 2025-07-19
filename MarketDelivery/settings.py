a580c1cd260ef41d7db60178166b25c0a73774a8
mohsen salari <72574557+mohsensalare@users.noreply.github.com>
2025-07-19 11:53:44 +0330

    Update settings.py
    update settings for vercel

--- MarketDelivery/settings.py
+++ MarketDelivery/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!#kdt@o%*3d6gxnw%av(t0&6zbd!_kyve6azvc&64-oy&bw)n)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "delivery-markets.herokuapp.com"]
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
@@ -92,23 +92,23 @@ WSGI_APPLICATION = 'MarketDelivery.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'd4iep8b61d5ee1',
-        'USER': 'zzoopwibdwupyx',
-        'PASSWORD': 'e5ee3fdff1aba74a2e214de6f39c9e2a4db3e861defaac31aeeae19e1a7bcaaa',
-        'HOST': 'ec2-3-222-204-187.compute-1.amazonaws.com',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'd4iep8b61d5ee1',
+#         'USER': 'zzoopwibdwupyx',
+#         'PASSWORD': 'e5ee3fdff1aba74a2e214de6f39c9e2a4db3e861defaac31aeeae19e1a7bcaaa',
+#         'HOST': 'ec2-3-222-204-187.compute-1.amazonaws.com',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
